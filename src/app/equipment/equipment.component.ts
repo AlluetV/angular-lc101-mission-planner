03b5eb7e5a5970d33b79632759a8add484419c95
AlluetV <alluetvalenzuela@gmail.com>
2021-08-09 19:33:29 -0500

    added crew and header

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+equipment:string[] = ["Habitat dome", "Drones", "Food-containers", "Oxygen-tanks"];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+ /*addEquipment(item: string){
+   this.equipment.push(item);
+ }*/
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-
+  experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density'];
   constructor() { }
 
   ngOnInit() {
